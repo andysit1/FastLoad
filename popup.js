7824a0b98dab1f9664df3b3bbb7dcb9f3027b727
Demon Lord <andysit173@gmail.com>
2023-02-17 00:31:30 -0500

    groups are showing on popup

--- popup.js
+++ popup.js
@@ -6,8 +6,8 @@ function getKeyByValue(object, value) {
 
 function update(){
 	const groupList = document.getElementById('groupList');
-
 	//Clear the groupings
+
 	var child = groupList.lastChild;
 	while(child){
 		groupList.removeChild(child)
@@ -15,40 +15,80 @@ function update(){
 	}
 
 	for (let [key, value] of groups){
-		console.log(key + " = " + value);
+		//collector of parts
+		const newGroup = document.createElement("div");
+		newGroup.setAttribute("class", "form-centerWrapper")
+		//group name
+		const groupName = document.createElement("div");
+		groupName.innerHTML = key;
+
+		//control buttons
+		const groupControls = document.createElement("div");
+
+		const buttonSave = document.createElement("button");
+        buttonSave.innerHTML = "Save (" + value.length + ")";
+        buttonSave.setAttribute("id", key);
+        buttonSave.setAttribute("class", "button");
+        buttonSave.addEventListener("click", (event) => save(event.target.id));
+
+		const buttonLoad = document.createElement("button");
+        buttonLoad.innerHTML = "Load";
+        buttonLoad.setAttribute("id", key);
+        buttonLoad.setAttribute("class", "button");
+        buttonLoad.addEventListener("click", (event) => load(event.target.id));
+		
+		
+		groupControls.appendChild(buttonSave);
+		groupControls.appendChild(buttonLoad);
+		// groupControls.appendChild(buttonLoad);
+
+		newGroup.appendChild(groupName);
+		newGroup.append(groupControls);
+
+		//inputs all into group list
+		groupList.append(newGroup);
 	}
 }
 
-function onLoaded(){
-	groups.set("1", []);
+function addGroup(){
+	var groupName = document.getElementById('groupName');
+	console.log("adding group to url management: " + groupName.value);
+	groups.set(groupName.value, []);
 
-	chrome.storage.sync.get('ChromeUrlSaver' , function (items){
-		var jsonString = items['ChromeUrlSaver'];
-		var urls =  JSON.parse(jsonString);
-	});
-
+	update();
 }
 
-function save() {
+
+function onLoaded(){
+	chrome.storage.sync.get("ManageTabUrl", function(items) {
+		for (let key in items) {
+			console.log(key, items[key])
+		}
+	});
+	update();
+}
+
+function save(id) {
+	console.log(id)
+	//get all tabs in current window
 	chrome.tabs.query({ currentWindow: true }, function(tabs) {
 	var urlArr = [];
 	for (var i = 0; i < tabs.length; i++) {
       urlArr.push(tabs[i].url);
     }
-	groups.set("1", urlArr);
-  });
+	//sets an id to urls in current window
+	groups.set(id, urlArr);
+  	});
+
+	//saves to google sync with the entries from Groups
+	chrome.storage.sync.set(
+		{ "ManageTabUrl": Object.fromEntries(groups) }
+	);
   update();
 }
 
-function load() {
-	// chrome.storage.sync.get('ChromeUrlSaver' , function (items){
-	// 	var jsonString = items['ChromeUrlSaver'];
-	// 	var urls =  JSON.parse(jsonString);
-	// 	for (var i = 0; i < urls.length; i++) {
-	// 		chrome.tabs.create({ url: urls[i] });
-	// 	}
-	// });
-	var urlArr = groups.get("1")
+function load(id) {
+	var urlArr = groups.get(id)
 	for (var i = 0; i < urlArr.length; i++){
 		chrome.tabs.create({ url : urlArr[i] });
 	}
@@ -57,10 +97,7 @@ function load() {
 
 window.onload = onLoaded;
 document.addEventListener('DOMContentLoaded', function() {
-	var buttonSave = document.getElementById('save_button');
-	buttonSave.addEventListener('click', save);
-	
-	var buttonLoad = document.getElementById('load_button');
-	buttonLoad.addEventListener('click', load);
+	var buttonSubmit = document.getElementById("submit-btn");
+	buttonSubmit.addEventListener('click', addGroup)
 });
-//buttons -> unqiue id -> returns a 
+
